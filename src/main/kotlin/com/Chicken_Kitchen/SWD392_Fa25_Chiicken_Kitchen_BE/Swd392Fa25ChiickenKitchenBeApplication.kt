b8212130f649f4d50408fef321c56df24e4e28c7
PawPasta <giabaostrike2004@gmail.com>
2025-09-18 07:32:38 +0700

    FIRST COMMIT

---
+++ src/main/kotlin/com/Chicken_Kitchen/SWD392_Fa25_Chiicken_Kitchen_BE/Swd392Fa25ChiickenKitchenBeApplication.kt
@@ -0,0 +1,11 @@
+package com.Chicken_Kitchen.SWD392_Fa25_Chiicken_Kitchen_BE
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Swd392Fa25ChiickenKitchenBeApplication
+
+fun main(args: Array<String>) {
+	runApplication<Swd392Fa25ChiickenKitchenBeApplication>(*args)
+}
---
+++ src/test/kotlin/com/Chicken_Kitchen/SWD392_Fa25_Chiicken_Kitchen_BE/Swd392Fa25ChiickenKitchenBeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.Chicken_Kitchen.SWD392_Fa25_Chiicken_Kitchen_BE
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Swd392Fa25ChiickenKitchenBeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
