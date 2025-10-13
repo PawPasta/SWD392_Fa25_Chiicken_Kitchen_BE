3b0baac04cbe0abeea052b3f0268221f7c45dfd8
PawPasta <giabaostrike2004@gmail.com>
2025-10-13 09:59:30 +0700

    update(properties) update application.properties

---
+++ src/main/kotlin/com/ChickenKitchen/app/model/entity/user/Wallet.kt
@@ -0,0 +1,27 @@
+package com.ChickenKitchen.app.model.entity.user
+
+import User
+import jakarta.persistence.*
+import java.sql.Timestamp
+
+@Entity
+@Table(name = "wallets")
+class Wallet(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false, unique = true)
+    val user: User,
+
+    @Column(nullable = false)
+    var balance: Int = 0,
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    val createdAt: Timestamp? = null,
+
+    @Column(name = "updated_at")
+    var updatedAt: Timestamp? = null,
+)
+
