b14b2698d13a0a27d1662c318f9ccb632db4a47a
PawPasta <giabaostrike2004@gmail.com>
2025-11-01 11:18:51 +0700

    feat(wallet) add mine wallet function

--- src/main/kotlin/com/ChickenKitchen/app/service/user/WalletService.kt
+++ src/main/kotlin/com/ChickenKitchen/app/service/user/WalletService.kt
@@ -1,9 +1,12 @@
 package com.ChickenKitchen.app.service.user
 
+import com.ChickenKitchen.app.model.dto.response.UserWalletResponse
 import com.ChickenKitchen.app.model.entity.user.User
 
 
 interface WalletService {
 
     fun deductFromWallet(user : User, amount: Int)
+
+    fun getMyWallet() : UserWalletResponse
 }
